04d1b9c7bb2a1e8e20f06e01cacdd0d5d079cd1e
Mateus Amorim <142228584+knalen25@users.noreply.github.com>
2025-03-04 15:23:02 -0300

    Add files via upload

---
+++ atividade1/variaveisOperadores.js
@@ -0,0 +1,23 @@
+
+let num_A = 10;
+let num_B = 5;
+let soma = num_A + num_B;
+console.log(`Resultado da soma entre ${num_A} e ${num_B} é ${soma}`);
+
+
+num_A = 15;
+num_B = 7;
+subtracao = num_A - num_B;
+console.log(`Resultado da subtração entre ${num_A} e ${num_B} é ${subtracao}`);
+
+
+num_A = 4;
+num_B = 3;
+multiplicacao = num_A * num_B;
+console.log(`Resultado da multiplicação entre ${num_A} e ${num_B} é ${multiplicacao}`);
+
+
+num_A = 20;
+num_B = 4;
+divisao = num_A / num_B;
+console.log(`Resultado da divisão entre ${num_A} e ${num_B} é ${divisao}`);
